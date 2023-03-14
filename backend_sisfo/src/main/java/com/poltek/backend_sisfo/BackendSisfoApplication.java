ad3b652f50bbef6c3445f7923838ff2894e9a96f
ikhsanramdhani <ikhsanuramdhani@gmail.com>
2023-03-14 07:59:34 +0700

    upload file sisfo

---
+++ backend_sisfo/src/main/java/com/poltek/backend_sisfo/BackendSisfoApplication.java
@@ -0,0 +1,13 @@
+package com.poltek.backend_sisfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSisfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSisfoApplication.class, args);
+	}
+
+}
---
+++ backend_sisfo/src/test/java/com/poltek/backend_sisfo/BackendSisfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poltek.backend_sisfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSisfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
