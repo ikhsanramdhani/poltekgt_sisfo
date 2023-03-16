c4ee54e5c7de3509ea5a02b49dfd845b7124aabf
Wisfie <wiz.fie@gmail.com>
2023-03-16 08:57:17 +0700

    sss

---
+++ frontend_sisfo/src/Pages/DosenPage.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="hm">
+		<Side></Side>
+		<div class="content">
+			<Title class="tt"></Title>
+			<Table class="tb"></Table>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Side from "@/components/side.vue";
+	import Title from "@/components/Title.vue";
+	import Table from "@/components/TableMhs.vue";
+	export default {
+		name: "DosenPage",
+		components: {
+			Title,
+			Side,
+			Table,
+		},
+	};
+</script>
+
+<style Scoped>
+	* {
+		margin: 0;
+	}
+	.hm {
+		min-height: 100vh;
+		width: 100vw;
+		display: flex;
+		margin: 0;
+	}
+	.content {
+		margin-left: 5%;
+		width: 140ex;
+	}
+
+	.tb {
+		width: 100rem;
+	}
+
+	.tt {
+		margin-top: 3%;
+		margin-bottom: 3%;
+	}
+</style>
---
+++ frontend_sisfo/src/components/TableMhs.vue
@@ -0,0 +1,49 @@
+<template>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th scope="col">Nim</th>
+				<th scope="col">Nama</th>
+				<th scope="col">Kelas</th>
+				<th scope="col">Jurusan</th>
+				<th scope="col">Nilai</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<td>Mark</td>
+				<td>Otto</td>
+				<td>@mdo</td>
+				<td><Button class="btn btn-primary">Ubah</Button></td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>Thornton</td>
+				<td>Jacob</td>
+				<td>@fat</td>
+				<td><Button class="btn btn-primary">Ubah</Button></td>
+			</tr>
+			<tr>
+				<th scope="row">3</th>
+				<td>Larry the Bird</td>
+				<td>@twitter</td>
+				<td>@twitter</td>
+				<td><Button class="btn btn-primary">Ubah</Button></td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+	export default {
+		name: "TableMh",
+	};
+</script>
+
+<style>
+	.table {
+		margin: auto;
+		width: 70vh;
+	}
+</style>
